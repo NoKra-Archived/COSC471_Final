062d8faaccfc1c6fbe30dea7771d18d7967a9c7e
Nakola <Kraus.N@protonmail.ch>
2023-04-20 09:32:37 -0400

    unfinished rotation implementation

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ verticies = (
     (1, 1, -1),
     (-1, 1, -1),
     (-1, -1, -1),
-    (1, -1, 1),
+    (2, -1, 1),
     (1, 1, 1),
     (-1, -1, 1),
     (-1, 1, 1)
@@ -44,17 +44,55 @@ def main():
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
+
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
 
-    glTranslatef(0.0,0.0, -5)
+    x_current = 0
+    y_current = 0
 
+    rotation_rate = 0.3
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        # Directional key rotation
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_LEFT]:
+            glRotatef(-1, 0, 1, 0)
+            x_current += -1
+            print("X: %d" % x_current)
+        if pressed_key[pygame.K_RIGHT]:
+            glRotatef(1, 0, 1, 0)
+            x_current += 1
+            print("X: %d" % x_current)
+        if pressed_key[pygame.K_DOWN]:
+            glRotatef(1, y_current, 0, 0)
+            y_current += 1
+            print("Y: %d" % y_current)
+        if pressed_key[pygame.K_UP]:
+            glRotatef(-1, y_current, 0, 0)
+            y_current -= 1
+            print("Y: %d" % y_current)
+
+        if x_current > 360 or x_current < -360:
+            x_current = 0
+        if y_current > 360 or y_current < -360:
+            y_current = 0
+
+        """ Mouse rotation implementation, fine tuning with keys
+        left, middle, right = pygame.mouse.get_pressed()
+        x_mouse, y_mouse = pygame.mouse.get_rel()
+        if left:
+            x_current += x_mouse
+            y_current += y_mouse
+            glRotatef(1 * (x_mouse * rotation_rate), 0, 1, 0)
+            glRotatef(1 * (y_mouse * rotation_rate), 0, 0, 1)
+            print("[%d , %d]" % (x_current, y_current))
+        """
+
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
