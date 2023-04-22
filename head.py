f2391781dfe243c6a0fdf1c44cfcf8d614a1fee8
Nakola <Kraus.N@protonmail.ch>
2023-04-22 19:47:42 -0400

    added temporary zoom in and out, more detail to printer head added

--- head.py
+++ head.py
@@ -9,7 +9,8 @@ class PrinterHead:
         self.main_box = self.__create_main_box()
         self.fan_box = self.__create_fan_box()
         self.nozzle = self.__create_nozzle()
-        self.all_parts = (self.main_box, self.fan_box, self.nozzle)
+        self.carriage_block = self.__create_carriage_block()
+        self.all_parts = (self.main_box, self.fan_box, self.nozzle, self.carriage_block)
 
     def __create_main_box(self):
         vertices = (
@@ -48,22 +49,22 @@ class PrinterHead:
         return vertices, edges
 
     def __create_fan_box(self):
-        fan_offset = self.head_height / 2.5
+        offset = self.head_height / 2.5 # Used for constant distance measurements
         # Box attachment points
         box_left_bottom_front = self.main_box[0][6]
         box_left_top_front = self.main_box[0][7]
         box_left_bottom_back = self.main_box[0][3]
         box_left_top_back = self.main_box[0][2]
         # Fan attachment points
-        bottom_front_connection = (box_left_bottom_front[0], box_left_bottom_front[1] + fan_offset, box_left_bottom_front[2] - fan_offset)
-        top_front_connection = (box_left_top_front[0], box_left_top_front[1] - fan_offset, box_left_top_front[2] - fan_offset)
-        bottom_back_connection = (box_left_bottom_back[0], box_left_bottom_back[1] + fan_offset, box_left_bottom_back[2] + fan_offset)
-        top_back_connection = (box_left_top_back[0], box_left_top_back[1] - fan_offset, box_left_top_back[2] + fan_offset)
+        bottom_front_connection = (box_left_bottom_front[0], box_left_bottom_front[1] + offset, box_left_bottom_front[2] - offset)
+        top_front_connection = (box_left_top_front[0], box_left_top_front[1] - offset, box_left_top_front[2] - offset)
+        bottom_back_connection = (box_left_bottom_back[0], box_left_bottom_back[1] + offset, box_left_bottom_back[2] + offset)
+        top_back_connection = (box_left_top_back[0], box_left_top_back[1] - offset, box_left_top_back[2] + offset)
         # Fan body points
-        bottom_front_fan = (bottom_front_connection[0] - fan_offset, bottom_front_connection[1], bottom_front_connection[2])
-        top_front_fan = (top_front_connection[0] - fan_offset, top_front_connection[1], top_front_connection[2])
-        bottom_back_fan = (bottom_back_connection[0] - fan_offset, bottom_back_connection[1], bottom_back_connection[2])
-        top_back_fan = (top_back_connection[0] - fan_offset, top_back_connection[1], top_back_connection[2])
+        bottom_front_fan = (bottom_front_connection[0] - offset, bottom_front_connection[1], bottom_front_connection[2])
+        top_front_fan = (top_front_connection[0] - offset, top_front_connection[1], top_front_connection[2])
+        bottom_back_fan = (bottom_back_connection[0] - offset, bottom_back_connection[1], bottom_back_connection[2])
+        top_back_fan = (top_back_connection[0] - offset, top_back_connection[1], top_back_connection[2])
 
         vertices = (
             box_left_bottom_front,  # 0
@@ -101,43 +102,58 @@ class PrinterHead:
         return vertices, edges
 
     def __create_nozzle(self):
-        attach_offset = self.head_length / 2.5
+        offset = self.head_length / 2.5
         # Box attachment points
         box_left_front = self.main_box[0][6]
         box_right_front = self.main_box[0][4]
         box_left_back = self.main_box[0][3]
         box_right_back = self.main_box[0][0]
         # Stem attachment points
-        left_front_attach = (box_left_front[0] / 2, box_left_front[1], box_left_front[2] - attach_offset)
-        right_front_attach = (box_right_front[0] / 2, box_right_front[1], box_right_front[2] - attach_offset)
-        left_back_attach = (box_left_back[0] / 2, box_left_back[1], left_front_attach[2] - attach_offset)
-        right_back_attach = (box_right_back[0] / 2, box_right_back[1], right_front_attach[2] - attach_offset)
+        left_front_attach = (box_left_front[0] / 2, box_left_front[1], box_left_front[2] - offset)
+        right_front_attach = (box_right_front[0] / 2, box_right_front[1], box_right_front[2] - offset)
+        left_back_attach = (box_left_back[0] / 2, box_left_back[1], left_front_attach[2] - offset)
+        right_back_attach = (box_right_back[0] / 2, box_right_back[1], right_front_attach[2] - offset)
         # Stem length points
-        left_front_stem = (left_front_attach[0], left_front_attach[1] - (attach_offset / 2), left_front_attach[2])
-        right_front_stem = (right_front_attach[0], right_front_attach[1] - (attach_offset / 2), right_front_attach[2])
-        left_back_stem = (left_back_attach[0], left_back_attach[1] - (attach_offset / 2), left_back_attach[2])
-        right_back_stem = (right_back_attach[0], right_back_attach[1] - (attach_offset / 2), right_back_attach[2])
+        left_front_stem = (left_front_attach[0], left_front_attach[1] - (offset / 2), left_front_attach[2])
+        right_front_stem = (right_front_attach[0], right_front_attach[1] - (offset / 2), right_front_attach[2])
+        left_back_stem = (left_back_attach[0], left_back_attach[1] - (offset / 2), left_back_attach[2])
+        right_back_stem = (right_back_attach[0], right_back_attach[1] - (offset / 2), right_back_attach[2])
         # Heat sink top points
         left_front_sink_top = (box_left_front[0], left_front_stem[1], box_left_front[2])
         right_front_sink_top = (box_right_front[0], right_front_stem[1], box_right_front[2])
-        left_back_sink_top = (box_left_back[0], left_back_stem[1], left_back_stem[2] - attach_offset)
-        right_back_sink_top = (box_right_back[0], right_back_stem[1], right_back_stem[2] - attach_offset)
+        left_back_sink_top = (box_left_back[0], left_back_stem[1], left_back_stem[2] - offset)
+        right_back_sink_top = (box_right_back[0], right_back_stem[1], right_back_stem[2] - offset)
         # Heat sink bottom points
-        left_front_sink_bottom = (left_front_sink_top[0], left_front_sink_top[1] - (attach_offset / 2), left_front_sink_top[2])
-        right_front_sink_bottom = (right_front_sink_top[0], right_front_sink_top[1] - (attach_offset / 2), right_front_sink_top[2])
-        left_back_sink_bottom = (left_back_sink_top[0], left_back_sink_top[1] - (attach_offset / 2), left_back_sink_top[2])
-        right_back_sink_bottom = (right_back_sink_top[0], right_back_sink_top[1] - (attach_offset / 2), right_back_sink_top[2])
-        left_mid_sink_bottom = (left_front_sink_bottom[0], left_back_sink_bottom[1] - (attach_offset / 2), left_back_stem[2])
-        right_mid_sink_bottom = (right_front_sink_bottom[0], right_back_sink_bottom[1] - (attach_offset / 2), right_back_stem[2])
+        left_front_sink_bottom = (left_front_sink_top[0], left_front_sink_top[1] - (offset / 2), left_front_sink_top[2])
+        right_front_sink_bottom = (right_front_sink_top[0], right_front_sink_top[1] - (offset / 2), right_front_sink_top[2])
+        left_back_sink_bottom = (left_back_sink_top[0], left_back_sink_top[1] - (offset / 2), left_back_sink_top[2])
+        right_back_sink_bottom = (right_back_sink_top[0], right_back_sink_top[1] - (offset / 2), right_back_sink_top[2])
+        left_mid_sink_bottom = (left_front_sink_bottom[0], left_back_sink_bottom[1] - (offset / 2), left_back_stem[2])
+        right_mid_sink_bottom = (right_front_sink_bottom[0], right_back_sink_bottom[1] - (offset / 2), right_back_stem[2])
         # Nozzle attachment points (y coordinates are temporary, may do some linear interpolation)
-        left_front_nozzle_top = (left_front_sink_bottom[0] + (attach_offset / 2), left_front_sink_bottom[1] - attach_offset / 3, left_front_sink_bottom[2] - (attach_offset / 2))
-        right_front_nozzle_top = (right_front_sink_bottom[0] - (attach_offset / 2), right_front_sink_bottom[1] - attach_offset / 3, right_front_sink_bottom[2] - (attach_offset / 2))
-        mid_front_nozzle_top = (0, left_front_sink_bottom[1] - attach_offset / 3, left_front_nozzle_top[2] + (attach_offset / 4))
+        left_front_nozzle_top = (left_front_sink_bottom[0] + (offset / 2), left_front_sink_bottom[1] - offset / 3, left_front_sink_bottom[2] - (offset / 2))
+        right_front_nozzle_top = (right_front_sink_bottom[0] - (offset / 2), right_front_sink_bottom[1] - offset / 3, right_front_sink_bottom[2] - (offset / 2))
+        mid_front_nozzle_top = (0, left_front_sink_bottom[1] - offset / 3, left_front_nozzle_top[2] + (offset / 4))
         mid_front_sink_attach = (0, left_front_sink_bottom[1], left_front_sink_bottom[2])
-        left_back_nozzle_top = (left_front_nozzle_top[0], left_front_nozzle_top[1], left_front_nozzle_top[2] - (attach_offset / 2))
-        right_back_nozzle_top = (right_front_nozzle_top[0], right_front_nozzle_top[1], right_front_nozzle_top[2] - (attach_offset / 2))
-        mid_back_nozzle_top = (0, left_front_nozzle_top[1], left_back_nozzle_top[2] - (attach_offset / 4))
+        left_back_nozzle_top = (left_front_nozzle_top[0], left_front_nozzle_top[1], left_front_nozzle_top[2] - (offset / 2))
+        right_back_nozzle_top = (right_front_nozzle_top[0], right_front_nozzle_top[1], right_front_nozzle_top[2] - (offset / 2))
+        mid_back_nozzle_top = (0, left_front_nozzle_top[1], left_back_nozzle_top[2] - (offset / 4))
         mid_back_sink_attach = (0, left_mid_sink_bottom[1], left_mid_sink_bottom[2])
+        # Nozzle body points
+        left_front_nozzle_bottom = (left_front_nozzle_top[0], left_front_nozzle_top[1] - (offset / 2), left_front_nozzle_top[2])
+        right_front_nozzle_bottom = (right_front_nozzle_top[0], right_front_nozzle_top[1] - (offset / 2), right_front_nozzle_top[2])
+        mid_front_nozzle_bottom = (0, mid_back_nozzle_top[1] - (offset / 2), mid_front_nozzle_top[2])
+        left_back_nozzle_bottom = (left_back_nozzle_top[0], left_back_nozzle_top[1] - (offset / 2), left_back_nozzle_top[2])
+        right_back_nozzle_bottom = (right_back_nozzle_top[0], right_back_nozzle_top[1] - (offset / 2), right_back_nozzle_top[2])
+        mid_back_nozzle_bottom = (0, mid_back_nozzle_top[1] - (offset / 2), mid_back_nozzle_top[2])
+        # Nozzle tip points
+        left_front_tip_attach = (left_front_nozzle_bottom[0] + (offset / 6), left_front_nozzle_bottom[1], left_front_nozzle_bottom[2] - (offset / 6))
+        right_front_tip_attach = (right_front_nozzle_bottom[0] - (offset / 6), right_front_nozzle_bottom[1], right_front_nozzle_bottom[2] - (offset / 6))
+        mid_front_tip_attach = (0, mid_front_nozzle_bottom[1], mid_front_nozzle_bottom[2] - (offset / 6))
+        left_back_tip_attach = (left_back_nozzle_bottom[0] + (offset / 6), left_back_nozzle_bottom[1], left_back_nozzle_bottom[2] + (offset / 6))
+        right_back_tip_attach = (right_back_nozzle_bottom[0] - (offset / 6), right_back_nozzle_bottom[1], right_back_nozzle_bottom[2] + (offset / 6))
+        mid_back_tip_attach = (0, mid_back_nozzle_bottom[1], mid_back_nozzle_bottom[2] + (offset / 6))
+        nozzle_tip = (0, mid_front_tip_attach[1] - (offset / 3), left_front_nozzle_bottom[2] - (offset / 4))
 
         vertices = (
             box_left_front,  # 0
@@ -170,7 +186,19 @@ class PrinterHead:
             right_back_nozzle_top,  # 27
             mid_back_nozzle_top,  # 28
             mid_back_sink_attach,  # 29
-
+            left_front_nozzle_bottom,  # 30
+            right_front_nozzle_bottom,  # 31
+            mid_front_nozzle_bottom,  # 32
+            left_back_nozzle_bottom,  # 33
+            right_back_nozzle_bottom,  # 34
+            mid_back_nozzle_bottom,  # 35
+            left_front_tip_attach,  # 36
+            right_front_tip_attach,  # 37
+            mid_front_tip_attach,  # 38
+            left_back_tip_attach,  # 39
+            right_back_tip_attach,  # 40
+            mid_back_tip_attach,  # 41
+            nozzle_tip,  # 42
         )
 
         edges = (
@@ -220,9 +248,101 @@ class PrinterHead:
             (23, 27),
             (26, 28),
             (27, 28),
-            (28, 29)
+            (28, 29),
+            (22, 30),
+            (23, 31),
+            (24, 32),
+            (26, 33),
+            (27, 34),
+            (28, 35),
+            (30, 32),
+            (31, 32),
+            (30, 33),
+            (31, 34),
+            (33, 35),
+            (34, 35),
+            (30, 36),
+            (31, 37),
+            (32, 38),
+            (33, 39),
+            (34, 40),
+            (35, 41),
+            (36, 38),
+            (37, 38),
+            (36, 39),
+            (37, 40),
+            (39, 41),
+            (40, 41),
+            (36, 42),
+            (37, 42),
+            (38, 42),
+            (39, 42),
+            (40, 42),
+            (41, 42)
 
         )
         return vertices, edges
 
 
+    def __create_carriage_block(self):
+        offset = self.head_length / 1.5
+        # Box attachment points
+        box_left_top_back = self.main_box[0][2]
+        box_right_top_back = self.main_box[0][1]
+        box_left_bottom_back = self.main_box[0][3]
+        box_right_bottom_back = self.main_box[0][0]
+        # Carriage body
+        body_left_top = (box_left_top_back[0], box_left_top_back[1], box_left_top_back[2] - offset)
+        body_mid_left_top_anchor = (box_left_top_back[0], box_left_top_back[1], box_left_top_back[2] - offset / 2)
+        body_right_top = (box_right_top_back[0], box_right_top_back[1], box_right_top_back[2] - offset)
+        body_mid_right_top_anchor = (box_right_top_back[0], box_right_top_back[1], box_right_top_back[2] - offset / 2)
+        body_left_bottom = (box_left_bottom_back[0], box_left_bottom_back[1], box_left_bottom_back[2] - offset)
+        body_mid_left_bottom_anchor = (box_left_bottom_back[0], box_left_bottom_back[1], box_left_bottom_back[2] - offset / 2)
+        body_right_bottom = (box_right_bottom_back[0], box_right_bottom_back[1], box_right_bottom_back[2] - offset)
+        body_mid_right_bottom_anchor = (box_right_bottom_back[0], box_right_bottom_back[1], box_right_bottom_back[2] - offset / 2)
+        # figure out the mid-point, probably height / 2 of the main box or something
+        body_left_mid_top_anchor = (body_left_top[0], body_left_top[1], body_left_top[2])
+        body_left_mid_bottom_anchor = (body_left_bottom[0], body_left_bottom[1], body_left_bottom[2])
+        body_right_mid_top_anchor = (body_right_top[0], body_right_top[1], body_right_top[2])
+        body_right_mid_bottom_anchor = (body_right_bottom[0], body_right_bottom[1], body_right_bottom[2])
+        # Guide rod bearing top (abbr. tbl = top bearing left, bbl = bottom bearing left, etc.)
+        tbl_mid_top_attach = (body_left_top[0], body_left_top[1] - offset / 3, body_left_top[2] + (offset / 2))
+        tbl_left_top_attach = (tbl_mid_top_attach[0], tbl_mid_top_attach[1] - offset / 2, tbl_mid_top_attach[2] - offset / 2)
+
+
+        vertices = (
+            box_left_top_back,  # 0
+            box_right_top_back,  # 1
+            box_left_bottom_back,  # 2
+            box_right_bottom_back,  # 3
+            body_left_top,  # 4
+            body_right_top,  # 5
+            body_left_bottom,  # 6
+            body_right_bottom,  # 7
+            body_mid_left_top_anchor,  # 8,
+            body_mid_right_top_anchor,  # 9
+            body_mid_left_bottom_anchor,  # 10
+            body_mid_right_bottom_anchor,  # 11
+            body_left_mid_top_anchor,  # 12
+            body_left_mid_bottom_anchor,  # 13
+            body_right_mid_top_anchor,  # 14
+            body_right_mid_bottom_anchor,  # 15
+            tbl_mid_top_attach,  # 16
+            tbl_left_top_attach,  # 17
+        )
+
+        edges = (
+            (0, 4),
+            (1, 5),
+            (2, 6),
+            (3, 7),
+            (4, 5),
+            (4, 6),
+            (5, 7),
+            (6, 7),
+            (8, 16),
+            (16, 17)
+
+        )
+
+        return vertices, edges
--- main.py
+++ main.py
@@ -5,100 +5,6 @@ from OpenGL.GLU import *
 
 from head import PrinterHead
 
-head_vertices = (
-    # Main box start
-    (1, -3, -3),  # right bottom back 0
-    (1, 2, -3),  # right top back 1
-    (-1, 2, -3),  # left top back 2
-    (-1, -3, -3),  # left bottom back 3
-    (1, -3, 2),  # right bottom front 4
-    (1, 2, 2),  # right top front 5
-    (-1, -3, 2),  # left bottom front 6
-    (-1, 2, 2),  # left top front 7
-    # Main box end
-    # Fan box start
-    (-1, 1, 1),  # top front attach 8
-    (-1, 1, -2),  # top back attach 9
-    (-1, -2, 1),  # bottom front attach 10
-    (-1, -2, -2),  # bottom back attach 11
-    (-2, 1, 1),  # top front box 12
-    (-2, 1, -2),  # top back box 13
-    (-2, -2, 1),  # bottom front box 14
-    (-2, -2, -2),  # bottom back box 15
-    # Fan box end
-    # Nozzle stem start
-    (-0.5, -3, 1),  # front left attach 16
-    (0.5, -3, 1),  # front right attach 17
-    (-0.5, -3, 0),  # back left attach 18
-    (0.5, -3, 0),  # back right attach 19
-    (-0.5, -3.5, 1),  # front left stem 20
-    (0.5, -3.5, 1),  # front right stem 21
-    (-0.5, -3.5, 0),  # back left stem 22
-    (0.5, -3.5, 0),  # back right stem 23
-    # Nozzle stem end
-    # Nozzle head start
-    (-1, -3.5, 2),  # front left top 24
-    (1, -3.5, 2),  # front right top 25
-    (-1, -3.5, -2),  # back left top 26
-    (1, -3.5, -2)  # back right top 27
-    )
-
-head_edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7),
-    (7, 8),
-    (2, 9),
-    (6, 10),
-    (3, 11),
-    (8, 9),
-    (8, 10),
-    (10, 11),
-    (9, 11),
-    (8, 12),
-    (9, 13),
-    (10, 14),
-    (11, 15),
-    (12, 13),
-    (12, 14),
-    (14, 15),
-    (13, 15),
-    (6, 16),
-    (4, 17),
-    (3, 18),
-    (0, 19),
-    (16, 17),
-    (17, 19),
-    (16, 18),
-    (18, 19),
-    (16, 20),
-    (17, 21),
-    (18, 22),
-    (19, 23),
-    (20, 21),
-    (20, 22),
-    (21, 23),
-    (22, 23),
-    (20, 24),
-    (21, 25),
-    (22, 26),
-    (23, 27),
-    (24, 25),
-    (24, 26),
-    (25, 27),
-    (26, 27)
-    )
-
-
 def Cube():
     glBegin(GL_LINES)
     head = PrinterHead(1, 5, 5)
@@ -115,16 +21,18 @@ def main():
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-    z_original = -12.0
-    y_original = 0.5
+    z_tran_start = -15.0
+    y_tran_start = 0.5
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0, y_original, z_original)
+    glTranslatef(0.0, y_tran_start, z_tran_start)
 
-    x_current = 0
-    y_current = 0
+    x_rot_current = 0
+    y_rot_current = 0
 
+    z_tran_current = z_tran_start
+    y_tran_current = y_tran_start
 
-    rotation_rate = 0.3
+    rot_rate = 1
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -135,46 +43,74 @@ def main():
                 if event.key == pygame.K_x:
                     glLoadIdentity()
                     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-                    glTranslatef(0.0, y_original, z_original)
-                    x_current = 0
-                    y_current = 0
-
+                    glTranslatef(0.0, y_tran_start, z_tran_start)
+                    x_rot_current = 0
+                    y_rot_current = 0
+            # Handles zoom in and zoom out (for now, only works when y rotation is 0)
+            if event.type == pygame.MOUSEWHEEL:
+                abs_x_rot = abs(x_rot_current)
+                if 0 <= abs_x_rot <= 89 or 181 <= abs_x_rot <= 269:
+                    modded_x = abs_x_rot % 90
+                    quadrant = 1 if abs_x_rot <= 90 else -1
+                    if event.y == -1:
+                        glTranslatef(quadrant * (modded_x / 90), 0, quadrant * (-(90 - modded_x) / 90))
+                    if event.y == 1:
+                        glTranslatef(quadrant * (-modded_x / 90), 0, quadrant * ((90 - modded_x) / 90))
+                elif abs_x_rot == 90:
+                    if event.y == -1:
+                        glTranslatef(1.0, 0.0, 0.0)
+                    if event.y == 1:
+                        glTranslatef(-1.0, 0.0, 0.0)
+                elif 91 <= abs_x_rot <= 179 or 271 <= abs_x_rot <= 359:
+                    modded_x = abs_x_rot % 90
+                    quadrant = 1 if abs_x_rot < 180 else -1
+                    if event.y == -1:
+                        glTranslatef(quadrant * ((90 - modded_x) / 90), 0, quadrant * (modded_x / 90))
+                    if event.y == 1:
+                        glTranslatef(quadrant * (-(90 - modded_x) / 90), 0, quadrant * (-modded_x / 90))
+                elif abs_x_rot == 180:
+                    if event.y == -1:
+                        glTranslatef(0.0, 0.0, 1.0)
+                    if event.y == 1:
+                        glTranslatef(0.0, 0.0, -1.0)
 
         # Directional key rotation
         pressed_key = pygame.key.get_pressed()
         if pressed_key[pygame.K_LEFT]:
-            glRotatef(-1, 0, 1, 0)
-            x_current += -1
-            print("X: %d" % x_current)
+            glRotatef(-rot_rate, 0, 1, 0)
+            x_rot_current -= rot_rate
+            print("X: %d" % x_rot_current)
         if pressed_key[pygame.K_RIGHT]:
-            glRotatef(1, 0, 1, 0)
-            x_current += 1
-            print("X: %d" % x_current)
+            glRotatef(rot_rate, 0, 1, 0)
+            x_rot_current += rot_rate
+            print("X: %d" % x_rot_current)
         if pressed_key[pygame.K_DOWN]:
-            #if y_current < 180:
-                glRotatef(1, 1, 0, 0)
-                y_current += 1
-                print("Y: %d" % y_current)
+            #if y_rot_current < 180:
+                glRotatef(rot_rate, 1, 0, 0)
+                y_rot_current += rot_rate
+                print("Y: %d" % y_rot_current)
         if pressed_key[pygame.K_UP]:
-            #if y_current >= 0:
-                glRotatef(-1, 1, 0, 0)
-                y_current -= 1
-                print("Y: %d" % y_current)
+            #if y_rot_current >= 0:
+                glRotatef(-rot_rate, 1, 0, 0)
+                y_rot_current -= rot_rate
+                print("Y: %d" % y_rot_current)
 
-        if x_current > 360 or x_current < -360:
-            x_current = 0
-        if y_current > 360 or y_current < -360:
-            y_current = 0
+        if x_rot_current >= 360:
+            x_rot_current = 0
+        if x_rot_current < 0:
+            x_rot_current = 359
+        if y_rot_current >= 360 or y_rot_current <= -360:
+            y_rot_current = 0
 
         """ Mouse rotation implementation, fine tuning with keys
         left, middle, right = pygame.mouse.get_pressed()
         x_mouse, y_mouse = pygame.mouse.get_rel()
         if left:
-            x_current += x_mouse
-            y_current += y_mouse
+            x_rot_current += x_mouse
+            y_rot_current += y_mouse
             glRotatef(1 * (x_mouse * rotation_rate), 0, 1, 0)
             glRotatef(1 * (y_mouse * rotation_rate), 0, 0, 1)
-            print("[%d , %d]" % (x_current, y_current))
+            print("[%d , %d]" % (x_rot_current, y_rot_current))
         """
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
