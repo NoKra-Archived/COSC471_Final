2a87b55ff072209450e89fab361580297df39a02
iansmith1337 <iansmith1337@gmail.com>
2023-05-02 11:17:14 -0400

    Update font_size

--- UI.py
+++ UI.py
@@ -6,8 +6,7 @@ from OpenGL.GLU import *
 
 
 def drawUIText(x, y, font_size, dh, dw, textString):
-    fs = font_size
-    font = pygame.font.Font("Fonts/FiraCode-VF.ttf", fs)
+    font = pygame.font.Font("Fonts/FiraCode-VF.ttf", font_size)
     render = font.render(
         textString, True, (255, 255, 255, 255), (0, 0, 0, 0)).convert_alpha()
     textData = pygame.image.tostring(render, "RGBA", True)
